76bd91cd211fa5f235a032d76217d02f1a814791
idojarks <idojarks@gmail.com>
2023-02-16 23:13:14 +0900

    Add project files.

---
+++ DreamKit/Contracts/Services/IPageService.cs
@@ -0,0 +1,6 @@
+﻿namespace DreamKit.Contracts.Services;
+
+public interface IPageService
+{
+    Type GetPageType(string key);
+}
---
+++ DreamKit/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DreamKit.ViewModels;
+
+public class MainViewModel : ObservableRecipient
+{
+    public MainViewModel()
+    {
+    }
+}
